f515d429cd5e730ae62e8cdcfbe2bcc5f50f850e
bara batta <34750743+barahijawi@users.noreply.github.com>
2022-11-26 23:19:16 -0600

    latest

--- rating-service/src/main/java/project/rating_service/controller/Controller.java
+++ rating-service/src/main/java/project/rating_service/controller/Controller.java
@@ -1,6 +1,7 @@
 package project.rating_service.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import project.rating_service.entity.Rating;
@@ -12,6 +13,7 @@ import java.util.Objects;
 
 @RestController
 @RequestMapping("/rating")
+@Slf4j
 public class Controller {
     @Autowired
     private RatingService ratingService;
@@ -20,6 +22,7 @@ public class Controller {
     @PostMapping("/add")
     public String addRating(@RequestBody Rating rating) {
         ratingService.addRating(rating);
+        log.info("Added rating");
         return "rating saved ";
     }
 
@@ -31,16 +34,19 @@ public class Controller {
 
     @GetMapping("/getbyuser/{id}")
     public List<Rating> getUserRatings(@PathVariable("id") Long userId) {
+       log.info("getting rating by user id");
         return ratingService.getRatingByUser(userId);
     }
 
     @DeleteMapping("/remove")
     public void removeRating(@PathVariable("id") Long ratingId){
+        log.info("removed rating id "+ratingId);
         ratingService.removeRating(ratingId);
     }
 
     @GetMapping("/averagerating/{id}")
     public double getAverageRating(@PathVariable("id") Long movieId){
+        log.info("calculating the average rating for movie/series id "+movieId);
         return Objects.isNull(Double.parseDouble
                 (df.format(ratingService.getAverageRating(movieId))))?
                 0.0:Double.parseDouble(df.format(ratingService.getAverageRating(movieId)));
